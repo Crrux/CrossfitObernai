11ea36d27670606c8a59beb2ed1c0403c93de1ea
Mursch Arthur <arthur.mursch@gmail.com>
2024-10-11 00:51:12 +0200

    modif animation open/close

--- src/components/popup_tel/popup_tel.jsx
+++ src/components/popup_tel/popup_tel.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
+
 function PopupTel() {
+  const [isOpen, setIsOpen] = useState(true);
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+  let isTabletorAbove = false;
+  const { innerWidth: width } = window;
+  if (width < 767.97) {
+    isTabletorAbove = true;
+  }
+
+  const handleClosePopup = () => {
+    setIsButtonClicked(true);
+    setTimeout(() => setIsOpen(false), 1000);
+  };
+
   return (
-    <aside>
-      <div>
-        <p>popup pc</p>
-      </div>
-    </aside>
+    <>
+      {isOpen && isTabletorAbove && (
+        <aside
+          className={`PopupTel ${isButtonClicked ? "close-animation" : ""}`}
+        >
+          <div>
+            <p>popup reservation tel</p>
+            <button className={`close-button`} onClick={handleClosePopup}>
+              X
+            </button>
+          </div>
+        </aside>
+      )}
+    </>
   );
 }
 
