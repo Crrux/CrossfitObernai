9f37d77992d8f0e437411cd999701e67d9de2bc8
Mursch Arthur <arthur.mursch@gmail.com>
2024-10-11 01:20:24 +0200

    handle link onclick

--- src/components/popup_pc/popup_pc.jsx
+++ src/components/popup_pc/popup_pc.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function PopupPC() {
   const [isOpen, setIsOpen] = useState(true);
@@ -21,10 +22,17 @@ function PopupPC() {
           className={`PopupPC ${isButtonClicked ? "close-animation-pc" : ""}`}
         >
           <div>
-            <button className={`close-button`} onClick={handleClosePopup}>
-              X
-            </button>
-            <p>popup reservation PC</p>
+            <header>
+              <button className={`close-button`} onClick={handleClosePopup}>
+                X
+              </button>
+            </header>
+            <main>
+              <Link to={"contact"} onClick={handleClosePopup}>
+                Lien contact
+              </Link>
+              <p>popup reservation PC</p>
+            </main>
           </div>
         </aside>
       )}
