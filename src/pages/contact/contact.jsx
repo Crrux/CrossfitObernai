a5b84de4a08d80502e343e1c7ef21be306d25c1f
Mursch Arthur <arthur.mursch@gmail.com>
2024-10-11 02:32:53 +0200

    save

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -2,6 +2,29 @@ function Contact() {
   return (
     <>
       <p>contact form</p>
+      <form action={import.meta.env.VITE_REACT_APP_API_URL} method="POST">
+        <label htmlFor="name">
+          <p>Nom</p>
+          <input type="text" id="name" name="name" required />
+        </label>
+        <label htmlFor="forname">
+          <p>Prenom</p>
+          <input type="text" id="forname" name="forname" required />
+        </label>
+        <label htmlFor="email">
+          <p>Email</p>
+          <input type="email" id="email" name="email" required />
+        </label>
+        <label htmlFor="tel">
+          <p>Telephone</p>
+          <input type="tel" id="tel" name="tel" required />
+        </label>
+        <label htmlFor="message">
+          <p>Message</p>
+          <textarea id="message" name="message" required />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </>
   );
 }
