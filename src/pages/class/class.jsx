9bb908df2a310ff31ed3e79e3fdf057d532e0c93
Mursch Arthur <arthur.mursch@gmail.com>
2024-10-31 19:29:32 +0100

    ajout numero titre collapsible

--- src/pages/class/class.jsx
+++ src/pages/class/class.jsx
@@ -5,7 +5,7 @@ function Class() {
     <main id="class">
       <h1>Nos Cours</h1>
       <Collapsible
-        titre={"Le WOD"}
+        titre={"1. Le WOD"}
         string={`Le WOD est l'acronyme de Workout of The Day. Chaque jour, une
           nouvelle séance où le but est de t'amener à travailler
           l'ensemble des qualités physiques au travers de 3 disciplines
@@ -13,14 +13,14 @@ function Class() {
         text={["le Cardio", "La Gymnastique", "L'haltérophilie"]}
       />
       <Collapsible
-        titre={"Functional Body Building"}
+        titre={"2. Functional Body Building"}
         string={`Notre séance de renforcement global,
           alliant exercices au poids de corps et à charges externes. Ses
           bienfaits sont la prévention de blessure, la réathlétisation et le
           développement de la masse musculaire.`}
       />
       <Collapsible
-        titre={"Gymnastique"}
+        titre={"3. Gymnastique"}
         string={`La gym' pratiquée en CrossFit regroupe tous les
           mouvements au poids de corps; tractions, pompes, montées de cordes,
           marche sur les mains et bien d'autres… Ce cours favorise le
@@ -29,7 +29,7 @@ function Class() {
           mouvements les plus simples aux plus complexes.`}
       />
       <Collapsible
-        titre={"Weighlifting"}
+        titre={"4. Weighlifting"}
         string={`Weightlifting est une discipline olympique composée de deux mouvements
           principaux : le snatch (arraché) et le clean & jerk (épaulé-jeté).
           Elle favorise le développement de la force, la vitesse,
@@ -38,7 +38,7 @@ function Class() {
           ciblé.`}
       />
       <Collapsible
-        titre={"Hybrid Training"}
+        titre={"5. Hybrid Training"}
         string={`Hybrid Training est un cours mélangeant la course à pieds à des
           exercices fonctionnels; vous y retrouverez la pratique du rameur, des
           wall ball shots, des fentes marchées, de la poussée et du tirage de
@@ -47,7 +47,7 @@ function Class() {
           cardiovasculaire et musculaire ciblée sur les membres inférieurs.`}
       />
       <Collapsible
-        titre={"CrossFit Kids / Teens"}
+        titre={"6. CrossFit Kids / Teens"}
         string={`CrossFit Kids / Teens est destiné aux enfants de 7 à 11 et de 12 à 17
           ans respectivement. C'est une façon ludique et amusante de faire
           de l'exercice pour vos enfants. Porter une charge avec une bonne
@@ -58,7 +58,7 @@ function Class() {
           détermination.`}
       />
       <Collapsible
-        titre={"Parents-enfants"}
+        titre={"7. Parents-enfants"}
         string={`Nous avons créé ce cours pour vous donner
           l'opportunité de partager un moment convivial et sportif en duo
           avec votre enfant. Son but est de renforcer vos liens dans un
@@ -67,7 +67,7 @@ function Class() {
           partir de 11 ans !`}
       />
       <Collapsible
-        titre={"Séniors"}
+        titre={"8. Séniors"}
         string={`Une pratique douce du cardio, de la souplesse et du
           renforcement musculaire pour repousser les effets indésirables de
           l'âge et conserver une santé de fer. En petits groupes, au
@@ -75,7 +75,7 @@ function Class() {
           chaleureuse. A partir de 65 ans !`}
       />
       <Collapsible
-        titre={"Le WoD Team"}
+        titre={"9. Le WoD Team"}
         string={`Le WoD Team est un rendez-vous à ne pas manquer ! Vous embarquez pour
           une aventure sportive à plusieurs, au travers d'exercices
           requérant la mise en place de stratégies de groupe en adéquation avec
