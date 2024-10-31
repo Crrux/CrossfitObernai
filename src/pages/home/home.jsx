b22a0d0ce791f625614e371ab83f28a94ad6d536
Mursch Arthur <arthur.mursch@gmail.com>
2024-10-31 18:26:11 +0100

    ajout texte

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,53 @@
 function Home() {
   return (
     <main className="Home">
-      <h2>Welcome to the home page!</h2>
-      <div>
-        <h2>Presentation box</h2>
-      </div>
+      <h1>CrossFit Obernai</h1>
+      <section>
+        <p>slides</p>
+      </section>
+      <section>
+        <h2>Qu&apos;est-ce que le CrossFit </h2>
+        <p>
+          Le CrossFit est un programme d&apos;entraînement général adapté à
+          tous, quelle que soit ta condition physique ou ton âge.
+        </p>
+        <p>
+          Il vise à améliorer ton état de santé global au travers
+          d&apos;entraînements fonctionnels où tous les mouvements sont basés
+          sur des mouvements naturels du quotidien.
+        </p>
+        <p>
+          Que tes objectifs soient une remise en forme, la préparation
+          d&apos;épreuves physiques pour un examen ou un challenge sportif,
+          notre méthode saura répondre favorablement à tes attentes.
+        </p>
+        <p>
+          Au travers de coachings personnalisés et adaptés, nous te corrigeons
+          sur chaque mouvement, en t&apos;assurant une pratique saine et
+          respectueuse de tes capacités ainsi qu&apos;une progression sans
+          limite grâce à une programmation variée.
+        </p>
+        <h2>Les points forts :</h2>
+        <ul>
+          <li>
+            La pratique en petits groupes sous l&apos;égide d&apos;un coach à
+            chaque séance
+          </li>
+          <li>Des séances variées, jamais deux fois le même entraînement !</li>
+          <li>
+            De multiples stimulis, le but est de t&apos;améliorer dans chaque
+            qualité physique reconnue dans le monde du sport
+          </li>
+          <li>
+            Un gros boost sur ta confiance en toi au travers de victoires
+            quotidiennes et d&apos;accomplissements aux entraînements
+          </li>
+          <li>
+            Une communauté bienveillante ou tout le monde s&apos;encourage et se
+            félicite
+          </li>
+        </ul>
+      </section>
     </main>
   );
 }
