fdde6475f13bad18d16e79cefffbdb1d6e416145
Mursch Arthur <arthur.mursch@gmail.com>
2024-10-31 18:46:45 +0100

    ajout composent collaps

---
+++ src/components/Collapsible/Collapsible.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+function Collapsible({ titre, text, string }) {
+  const [isActive, setIsActive] = useState(false);
+  const [isFirstClick, setIsFirstClick] = useState(true);
+  const handleClick = () => {
+    setIsActive(!isActive);
+    if (isFirstClick) {
+      setIsFirstClick(false);
+    }
+  };
+  return (
+    <div className="collapsible">
+      <div
+        className={`collapsible-divbutton ${
+          !isFirstClick ? (isActive ? "active" : "inactive") : ""
+        }`}
+      >
+        <p>{titre}</p>
+        <button
+          type="button"
+          className={`collapsible-button ${isActive ? "active" : ""}`}
+          onClick={handleClick}
+        >
+          <i
+            id="icon"
+            className={`fa-solid fa-chevron-up ${
+              !isFirstClick
+                ? isActive
+                  ? "icon-down-rotated"
+                  : "icon-up-rotated"
+                : ""
+            }`}
+          ></i>
+        </button>
+      </div>
+
+      <div
+        className={`collapsible-content ${
+          !isFirstClick ? (isActive ? "active" : "inactive") : ""
+        }`}
+      >
+        {text ? (
+          <>
+            {text.map((text) => (
+              <p key={text}>{text}</p>
+            ))}
+          </>
+        ) : (
+          ""
+        )}
+        {string ? (
+          <>
+            <p>{string}</p>
+          </>
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
+  );
+}
+
+Collapsible.propTypes = {
+  titre: PropTypes.string,
+  string: PropTypes.string,
+  text: PropTypes.array,
+};
+
+export default Collapsible;
