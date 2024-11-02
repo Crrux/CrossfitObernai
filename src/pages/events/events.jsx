4f69c89249372a79221a6cbb117c1ae0f19a1059
Mursch Arthur <arthur.mursch@gmail.com>
2024-11-02 14:19:15 +0100

    ajout texte

--- src/pages/events/events.jsx
+++ src/pages/events/events.jsx
@@ -1,7 +1,28 @@
 function Events() {
   return (
     <>
-      <p>evenements</p>
+      <h1>Nos evenements</h1>
+      <p>
+        Toute l&apos;année, nos événements viennent dynamiser la vie de la box.
+        A quoi vous attendre ?
+      </p>
+      <ul>
+        <li>
+          Des repas et barbecues pour les fêtes et parfois sans raison
+          particulière
+        </li>
+        <li>
+          Une compétition par équipe ouverte à tous, les &quot;
+          <a
+            style={{ textDecoration: "none" }}
+            href="https://www.instagram.com/heroesteamcontest/"
+          >
+            Heroes Team Contest
+          </a>
+          &quot; pour vous challenger auprès de concurrents venus des quatre
+          coins de la France !
+        </li>
+      </ul>
     </>
   );
 }
