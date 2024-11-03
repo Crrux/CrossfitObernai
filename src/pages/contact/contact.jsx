ea744249088f5f096e217818b087c5d15c71e710
Mursch Arthur <arthur.mursch@gmail.com>
2024-11-03 13:40:52 +0100

    ajout version pc/tel pour infos

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -13,6 +13,11 @@ function Contact() {
     message: "",
   });
 
+  let isTabletorAbove = false;
+  const { innerWidth: width } = window;
+  if (width <= 767.97) {
+    isTabletorAbove = true;
+  }
   // Initialize state for form validation errors
   const [hasErrors, setHasErrors] = useState(true);
 
@@ -168,41 +173,67 @@ function Contact() {
         </form>
         <section id="Contact_info">
           <div>
-            <p>
-              Adresse:
-              <a
-                href="https://maps.app.goo.gl/8VZHC6K4swqYMEe4A"
-                target="_blank"
-                rel="noreferrer"
-              >
-                4 rue du Thal, 67210 Obernai
-              </a>
-            </p>
-            <p>
-              Téléphone: <a href="tel:+33614030694">06.14.03.06.94</a>
-            </p>
-            <p>
-              Email:
-              <a href="mailto:crossfitobernai@gmail.com">
-                crossfitobernai@gmail.com
-              </a>
-            </p>
-            <div id="Contact_info_Reseaux">
-              <a
-                href="https://www.facebook.com/cfobernai/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img src={LogoFacebook} alt="Lien Facebook"></img>
-              </a>
-              <a
-                href="https://www.instagram.com/crossfitobernai/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img src={LogoInstagram} alt="Lien Instagram" />
-              </a>
-            </div>
+            {isTabletorAbove ? (
+              <>
+                <p>
+                  Adresse:
+                  <a
+                    href="https://maps.app.goo.gl/8VZHC6K4swqYMEe4A"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    4 rue du Thal, 67210 Obernai
+                  </a>
+                </p>
+                <p>
+                  Téléphone: <a href="tel:+33614030694">06.14.03.06.94</a>
+                </p>
+                <p>
+                  Email:
+                  <a href="mailto:crossfitobernai@gmail.com">
+                    crossfitobernai@gmail.com
+                  </a>
+                </p>
+                <div id="Contact_info_Reseaux">
+                  <a
+                    href="https://www.facebook.com/cfobernai/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img src={LogoFacebook} alt="Lien Facebook"></img>
+                  </a>
+                  <a
+                    href="https://www.instagram.com/crossfitobernai/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img src={LogoInstagram} alt="Lien Instagram" />
+                  </a>
+                </div>
+              </>
+            ) : (
+              <>
+                <p>Adresse: 4 rue du Thal, 67210 Obernai</p>
+                <p>Téléphone: 06.14.03.06.94</p>
+                <p>Email: crossfitobernai@gmail.com</p>
+                <div id="Contact_info_Reseaux">
+                  <a
+                    href="https://www.facebook.com/cfobernai/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img src={LogoFacebook} alt="Lien Facebook"></img>
+                  </a>
+                  <a
+                    href="https://www.instagram.com/crossfitobernai/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img src={LogoInstagram} alt="Lien Instagram" />
+                  </a>
+                </div>
+              </>
+            )}
           </div>
           <iframe
             width="450"
