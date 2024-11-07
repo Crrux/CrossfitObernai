9d99b99747944cd547b7c9cb0b515b67fa363aed
Mursch Arthur <arthur.mursch@gmail.com>
2024-11-07 16:01:20 +0100

    integration dans header + style

--- src/components/BurgerNav/BurgerNav.jsx
+++ src/components/BurgerNav/BurgerNav.jsx
@@ -1,77 +1,76 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import LogoHeader from "/assets/Header_logo.png";
 
 function BurgerNav() {
   const [isOpen, setIsOpen] = useState(false);
   const handleShowLinks = () => setIsOpen(!isOpen);
   return (
     <nav className={`navbar ${isOpen ? "show-nav" : "hide-nav"}`}>
-      <div className="navbar__logo">
-        <ul className="navbar__links">
-          <li className="navbar__item">
-            <Link to={"/"} className={`navbar__link`} onClick={handleShowLinks}>
-              Acceuil
-            </Link>
-          </li>
-          <li className="navbar__item">
-            <Link
-              to={"class"}
-              className={`navbar__link`}
-              onClick={handleShowLinks}
-            >
-              Nos Cours
-            </Link>
-          </li>
-          <li className="navbar__item">
-            <Link
-              to={"planning"}
-              className={`navbar__link`}
-              onClick={handleShowLinks}
-            >
-              Planning
-            </Link>
-          </li>
-          <li className="navbar__item">
-            <Link
-              to={"offres"}
-              className={`navbar__link`}
-              onClick={handleShowLinks}
-            >
-              Les offres
-            </Link>
-          </li>
-          <li className="navbar__item">
-            <Link
-              to={"contact"}
-              className={`navbar__link`}
-              onClick={handleShowLinks}
-            >
-              Contact
-            </Link>
-          </li>
-          <li className="navbar__item">
-            <Link
-              to={"events"}
-              className={`navbar__link`}
-              onClick={handleShowLinks}
-            >
-              Evenements
-            </Link>
-          </li>
-          <li className="navbar__item">
-            <Link
-              to={"testerreur"}
-              className={`navbar__link`}
-              onClick={handleShowLinks}
-            >
-              Test erreur
-            </Link>
-          </li>
-        </ul>
-        <button className="navbar__burger" onClick={handleShowLinks}>
-          <span className="burger-bar"></span>
-        </button>
-      </div>
+      <ul className="navbar__links">
+        <li className="navbar__item">
+          <Link to={"/"} className={`navbar__link`} onClick={handleShowLinks}>
+            Acceuil
+          </Link>
+        </li>
+        <li className="navbar__item">
+          <Link
+            to={"class"}
+            className={`navbar__link`}
+            onClick={handleShowLinks}
+          >
+            Nos Cours
+          </Link>
+        </li>
+        <li className="navbar__item">
+          <Link
+            to={"planning"}
+            className={`navbar__link`}
+            onClick={handleShowLinks}
+          >
+            Planning
+          </Link>
+        </li>
+        <li className="navbar__item">
+          <Link
+            to={"offres"}
+            className={`navbar__link`}
+            onClick={handleShowLinks}
+          >
+            Les offres
+          </Link>
+        </li>
+        <li className="navbar__item">
+          <Link
+            to={"contact"}
+            className={`navbar__link`}
+            onClick={handleShowLinks}
+          >
+            Contact
+          </Link>
+        </li>
+        <li className="navbar__item">
+          <Link
+            to={"events"}
+            className={`navbar__link`}
+            onClick={handleShowLinks}
+          >
+            Evenements
+          </Link>
+        </li>
+        <li className="navbar__item">
+          <Link
+            to={"testerreur"}
+            className={`navbar__link`}
+            onClick={handleShowLinks}
+          >
+            Test erreur
+          </Link>
+        </li>
+      </ul>
+      <button className="navbar__burger" onClick={handleShowLinks}>
+        <span className="burger-bar"></span>
+      </button>
     </nav>
   );
 }
--- src/layout/header/header.jsx
+++ src/layout/header/header.jsx
@@ -1,49 +1,65 @@
 import { Link, useLocation } from "react-router-dom";
 import Logo from "/assets/Header_logo.png";
 
+import BurgerNav from "../../components/BurgerNav/BurgerNav";
+
 function Header() {
   const location = useLocation();
-  console.log(location.pathname);
+  let isTabletorAbove = false;
+  const { innerWidth: width } = window;
+  if (width <= 767.97) {
+    isTabletorAbove = true;
+  }
   return (
     <header>
-      <div className="logo">
-        <Link to={"/"}>
-          <img src={Logo} alt="Logo CrossFit Obernai" id="Header_Logo" />
-        </Link>
-      </div>
-      <nav>
-        <Link
-          to={"class"}
-          className={location.pathname === "/class" ? "activeNavLink" : ""}
-        >
-          Nos cours
-        </Link>
-        <Link
-          to={"planning"}
-          className={location.pathname === "/planning" ? "activeNavLink" : ""}
-        >
-          Planning
-        </Link>
-        <Link
-          to={"offres"}
-          className={location.pathname === "/offres" ? "activeNavLink" : ""}
-        >
-          Les offres
-        </Link>
-        <Link
-          to={"contact"}
-          className={location.pathname === "/contact" ? "activeNavLink" : ""}
-        >
-          Contact
-        </Link>
-        <Link
-          to={"events"}
-          className={location.pathname === "/events" ? "activeNavLink" : ""}
-        >
-          Evenements
-        </Link>
-        <Link to={"testerreur"}>Test erreur</Link>
-      </nav>
+      {isTabletorAbove ? (
+        <BurgerNav />
+      ) : (
+        <>
+          <div className="logo">
+            <Link to={"/"}>
+              <img src={Logo} alt="Logo CrossFit Obernai" id="Header_Logo" />
+            </Link>
+          </div>
+          <nav id="NavPc">
+            <Link
+              to={"class"}
+              className={location.pathname === "/class" ? "activeNavLink" : ""}
+            >
+              Nos cours
+            </Link>
+            <Link
+              to={"planning"}
+              className={
+                location.pathname === "/planning" ? "activeNavLink" : ""
+              }
+            >
+              Planning
+            </Link>
+            <Link
+              to={"offres"}
+              className={location.pathname === "/offres" ? "activeNavLink" : ""}
+            >
+              Les offres
+            </Link>
+            <Link
+              to={"contact"}
+              className={
+                location.pathname === "/contact" ? "activeNavLink" : ""
+              }
+            >
+              Contact
+            </Link>
+            <Link
+              to={"events"}
+              className={location.pathname === "/events" ? "activeNavLink" : ""}
+            >
+              Evenements
+            </Link>
+            <Link to={"testerreur"}>Test erreur</Link>
+          </nav>
+        </>
+      )}
     </header>
   );
 }
