673bf410b91cc20b6cb8da98b543ac6ea2c3d688
Mursch Arthur <arthur.mursch@gmail.com>
2024-11-08 14:31:40 +0100

    Creation Loading + import

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,14 @@
+function Loading() {
+  return (
+    <div className="wrapper">
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="shadow"></div>
+      <div className="shadow"></div>
+      <div className="shadow"></div>
+    </div>
+  );
+}
+
+export default Loading;
