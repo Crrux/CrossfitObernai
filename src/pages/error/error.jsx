b31ad18d0474c13e9230c48288ae903028e12549
Mursch Arthur <arthur.mursch@gmail.com>
2024-11-19 16:59:11 +0100

    retour auto a l'acceuil en cas d'erreur

--- src/pages/error/error.jsx
+++ src/pages/error/error.jsx
@@ -1,7 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import Loading from "../../components/Loading/Loading";
+
 function Error() {
+  const navigate = useNavigate();
+
+  setTimeout(() => {
+    navigate("/");
+  }, 4000);
+
   return (
-    <main>
+    <main id="main_error">
       <p>404 error</p>
+      <p>La page demander n&apos;est pas disponible</p>
+      {/* <p>Retour a l&apos;acceuil</p> */}
+      <Loading />
     </main>
   );
 }
