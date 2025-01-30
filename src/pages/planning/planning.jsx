ee8c9d9485f1b87d32530686fdea3208aa070e33
Mursch Arthur <arthur.mursch@gmail.com>
2025-01-30 21:25:31 +0100

    save

--- src/pages/planning/planning.jsx
+++ src/pages/planning/planning.jsx
@@ -1,69 +1,7 @@
 import TitleBackgroundImage from "/assets/title_background/TitleBackground_Planning.webp";
 import PlanningImg from "/assets/Planning/Planning_CFO_2025.webp"
-// import { useRef, useState } from "react";
-// import styled from "styled-components";
-
-// const Container = styled.div`
-//   position: relative;
-//   overflow: hidden;
-//   display: block;
-//   border-radius: 15px;
-//   :hover {
-//     box-shadow: 0 14px 24px rgba(0, 0, 0, 0.55), 0 14px 18px rgba(0, 0, 0, 0.55);
-//   }
-// `;
-
-// const Image = styled.img.attrs((props) => ({
-//   src: props.source
-// }))``;
-
-// const Target = styled(Image)`
-//   position: absolute;
-//   left: ${(props) => props.offset.left}px;
-//   top: ${(props) => props.offset.top}px;
-//   opacity: ${(props) => props.opacity};
-// `;
 
 function Planning() {
-
-  // const sourceRef = useRef(null);
-  // const targetRef = useRef(null);
-  // const containerRef = useRef(null);
-
-  // const [opacity, setOpacity] = useState(0);
-  // const [offset, setOffset] = useState({ left: 0, top: 0 });
-
-  // const handleMouseEnter = () => {
-  //   setOpacity(1);
-  // };
-
-  // const handleMouseLeave = () => {
-  //   setOpacity(0);
-  // };
-
-  // const handleMouseMove = (e) => {
-  //   const targetRect = targetRef.current.getBoundingClientRect();
-  //   const sourceRect = sourceRef.current.getBoundingClientRect();
-  //   const containerRect = containerRef.current.getBoundingClientRect();
-
-  //   const xRatio = (targetRect.width - containerRect.width) / sourceRect.width;
-  //   const yRatio =
-  //     (targetRect.height - containerRect.height) / sourceRect.height;
-
-  //   const left = Math.max(
-  //     Math.min(e.pageX - sourceRect.left, sourceRect.width),
-  //     0
-  //   );
-  //   const top = Math.max(
-  //     Math.min(e.pageY - sourceRect.top, sourceRect.height),
-  //     0
-  //   );
-
-  //   setOffset({
-  //     left: left * -xRatio,
-  //     top: top * -yRatio
-  //   });
-  // };
 
   return (
     <main className="Planning">
