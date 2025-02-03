a0ed85336dee3c8b19a03fd7c3e5d0131f3fb4b8
Mursch Arthur <arthur.mursch@gmail.com>
2025-02-03 21:35:48 +0100

    ajout mentions legales

--- src/layout/legal_notices/legal_notices.jsx
+++ src/layout/legal_notices/legal_notices.jsx
@@ -3,7 +3,32 @@ function LegalNotices() {
         <>
             <h1>Mentions légales</h1>
             <h2>Editeur du site</h2>
-            <p></p>
+            <p>Le site CrossFit Obernai est édité par Egide Concept, une Société par Actions Simplifiée (SAS) au capital de 5000 euros, dont le siège social est situé au 4 rue du Thal, 67210 Obernai - FRANCE, immatriculée au Registre du Commerce et des Sociétés de Saverne sous le numéro Saverne B 893655845</p>
+            <p>Pour toute question ou demande concernant le site, vous pouvez contacter l'éditeur à l'adresse suivante :</p>
+            <ul>
+                <li>E-mail: crossfitobernai@gmail.com</li>
+                <li>Téléphone: +33 (0)6 14 03 06 94</li>
+            </ul>
+            <h2>Activités réglementées</h2>
+            <p>Mise à disposition d'un espace et d'équipements aux membres pour la pratique d'activités physiques, fitness. La commercialisation de tous produits et services se rapportant à l'objet social, toutes activités s'y rapportant.</p>
+            <h2>Directeur de la publication</h2>
+            <p>Le directeur de la publication est Damien Kiefer, en qualité de Président.</p>
+            <h2>Hébergement</h2>
+            <p>tbd</p>
+            <h2>Conception et réalisation</h2>
+            <p>Le site a été conçu et réalisé par Mursch Arthur. Pour toute question ou demande d'information, vous pouvez nous contacter à l'adresse suivante : arthur.mursch@gmail.com</p>
+            <h2>Données personnelles</h2>
+            <p>Le site crossfitobernai.com ne collecte aucune donnée personnelle à l'exception des informations fournies volontairement par les utilisateurs via le formulaire de contact. Les informations collectées sont : nom, prénom, adresse e-mail, numéro de téléphone et message. Ces données sont utilisées uniquement pour répondre aux demandes des utilisateurs et ne sont pas transmises à des tiers sans leur consentement préalable.</p>
+            <p>Les données personnelles collectées via le formulaire de contact (nom, prénom, adresse e-mail, numéro de téléphone et message) peuvent être utilisées à des fins publicitaires par Egide Concept, sous réserve de votre consentement explicite. En cochant la case de consentement dans le formulaire de contact, vous acceptez que vos données personnelles soient utilisées pour vous envoyer des communications commerciales et des offres promotionnelles.</p>
+            <p>Vos données personnelles seront conservées pendant une durée de 3 ans à compter de leur collecte, sauf si vous exercez votre droit de suppression ou d'opposition avant l'expiration de cette période.</p>
+            <p>Conformément à la loi "Informatique et Libertés" du 6 janvier 1978 modifiée et au Règlement Général sur la Protection des Données (RGPD), vous disposez d'un droit d'accès, de rectification, de suppression et d'opposition aux données vous concernant. Pour exercer ces droits, vous pouvez contacter Egide Concept à l'adresse suivante : crossfitobernai@gmail.com</p>
+            <h2>Cookies</h2>
+            <p>Le site crossfitobernai.com n'utilise pas de cookies.</p>
+            <h2>Propriété intellectuelle</h2>
+            <p>Tous les éléments du site crossfitobernai.com, y compris les textes, images, graphiques, logos, icônes, sons et logiciels, sont la propriété exclusive de Egide Concept ou de ses partenaires. Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de Egide Concept.</p>
+            <h2>Responsabilité</h2>
+            <p>Egide Concept s'efforce de fournir des informations aussi précises que possible. Toutefois, il ne pourra être tenu responsable des omissions, des inexactitudes et des carences dans la mise à jour, qu'elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces informations.</p>
+            <p>Toutes les informations indiquées sur le site CrossFit Obernai sont données à titre indicatif, et sont susceptibles d'évoluer. Par ailleurs, les renseignements figurant sur le site CrossFit Obernai ne sont pas exhaustifs. Ils sont donnés sous réserve de modifications ayant été apportées depuis leur mise en ligne.</p>
         </>
     )
 }
